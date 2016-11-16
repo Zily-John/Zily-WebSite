0835f771cd04995ffbc4610e70d3b97a9e9d4f6d
Zily <243865390@qq.com>
2016-11-16 14:12:15 +0800

    add config file

--- www/app.py
+++ www/app.py
@@ -1,12 +1,18 @@
 import logging; logging.basicConfig(level=logging.INFO)
-
 import asyncio,os,json,time
 from  datetime import datetime
-
 from aiohttp import web
+import orm,sys
+from coroweb import get, add_route, add_static
+from jinja2 import environment,FileSystemLoader
 
+@get('/')
 def index(request):
-    return web.Response(body=b'Hi,Every One!')
+    users = yield from User.findAll()
+    return {
+        '__template__': 'test.html',
+        'user': users
+    }
 
 @asyncio.coroutine
 def init(loop):
@@ -16,7 +22,8 @@ def init(loop):
     logging.info("server started at http://127.0.0.1:9000...")
     return srv
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init(loop))
+    loop.run_forever()
 
---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configuratuons
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'password': '',
+    'db': 'ZilyWeb'
+    },
+    'session': {
+        'secret': 'ZilyWeb'
+    }
+}
